e1d5087b4c36adc98a44227fdfac952853b08b6e
prajwal <prajwaljcloud@gmail.com>
2026-01-21 23:04:05 +0530

    Created contact support team component in pricing

--- client/src/Pages/Pricing.jsx
+++ client/src/Pages/Pricing.jsx
@@ -4,31 +4,53 @@ const Pricing = () => {
     return (
         <div className='container mx-auto mb-20'>
             <h1 className='text-xl font-bold text-center mb-8'>Pricing</h1>
-            <div className='flex items-start justify-between'>
+            <div className='flex items-start justify-between mb-16'>
                 <div className='border rounded-2xl py-4 px-12 w-104 hover:shadow-md'>
                     <h4 className='text-lg font-bold text-center mb-4'>Free</h4>
                     <ul className='list-disc'>
                         <li>Summarize PDFs upto 5 pages/day.</li>
                         <li>Max file size 20 MB.</li>
                         <li>Standard AI model.</li>
+                        <li>Voice Support.</li>
                     </ul>
-                    <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2'>Login In</button>
+                    <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2 cursor-pointer'>Login Now</button>
                 </div>
                 <div className='border rounded-2xl py-4 px-12 w-104 hover:shadow-md relative'>
                     <h4 className='text-lg font-bold text-center mb-4'>Basic <span className='bg-blue-500 text-base font-medium px-2 py-1 rounded-b-md absolute -top-1 right-5 shadow-md shadow-gray-400 text-white '>Most Popular</span></h4>
                     <ul className='list-disc'>
                         <li>Summarize PDFs upto 500 pages/day.</li>
                         <li>Max file size upto 1 GB.</li>
+                        <li>Access out Legatrix Max AI model.</li>
+                        <li>Voice support + AI conversation.</li>
+                        <li>Add multiple users (max 2).</li>
                     </ul>
-                    <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2'>Login In</button>
+                    <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2 cursor-pointer'>Login Now</button>
                 </div>
                 <div className='border rounded-2xl py-4 px-12 w-104 hover:shadow-md'>
                     <h4 className='text-lg font-bold text-center mb-4'>Professional</h4>
                     <ul className='list-disc'>
                         <li>Summarize PDFs upto 50,000 pages/day.</li>
                         <li>Max file size upto 25 GB.</li>
+                        <li>Access out Legatrix Max AI model.</li>
+                        <li>Voice support + AI conversation.</li>
+                        <li>Add multiple users(max 15).</li>
                     </ul>
-                    <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2'>Login In</button>
+                    <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2 cursor-pointer'>Login Now</button>
+                </div>
+            </div>
+            <div>
+                <h1 className='text-xl font-bold mb-8'>Not the plan your looking for?</h1>
+                <div className='w-full border rounded-2xl flex flex-col items-center justify-between py-16 mb-8'>
+                    <h4 className='text-lg font-bold mb-8'>Contact Support Team</h4>
+                    <p className='w-120 mb-2'><span className='text-xl text-red-600'>* </span>Name: </p>
+                    <input type="text" name="name" id="name" className='border border-gray-400 h-12 w-120 rounded-lg px-4 mb-4' required />
+                    <p className='w-120 mb-2'><span className='text-xl text-red-600'>* </span>Email: </p>
+                    <input type="email" name="email" id="email" className='border border-gray-400 h-12 w-120 rounded-lg px-4 mb-4' required />
+                    <p className='w-120 mb-2'><span className='text-xl text-red-600'>* </span>Detailed Query: </p>
+                    <textarea name="query" id="query" rows='8' cols='59' className='border border-gray-400 rounded-lg px-4 py-2 mb-4'></textarea>
+                    <p className='w-118 mt-8'>Or else you can contact us from below email id or mobile number:</p>
+                    <p className='w-118 mt-4'>Email id: <b>contactus@ailegatrix</b></p>
+                    <p className='w-118 mt-4'>Contact number: <b>+91 1234567890</b></p>
                 </div>
             </div>
         </div>
