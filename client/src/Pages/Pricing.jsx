e39802a4a0e950475f5e2354c03cb9ee0cfa3cb7
prajwal <prajwaljcloud@gmail.com>
2026-01-25 22:13:22 +0530

    created mobile version of pricing page

--- client/src/Pages/Pricing.jsx
+++ client/src/Pages/Pricing.jsx
@@ -4,8 +4,8 @@ const Pricing = () => {
     return (
         <div className='container mx-auto mb-20'>
             <h1 className='text-xl font-bold text-center mb-8'>Pricing</h1>
-            <div className='flex items-start justify-between mb-16'>
-                <div className='border rounded-2xl py-4 px-12 w-104 hover:shadow-md'>
+            <div className='w-full mx-auto flex flex-col items-center justify-center gap-10 lg:flex-row lg:items-start lg:justify-between lg:gap-0 mb-16'>
+                <div className='border rounded-2xl py-4 px-12 w-84 lg:w-104 hover:shadow-md'>
                     <h4 className='text-lg font-bold text-center mb-4'>Free</h4>
                     <ul className='list-disc'>
                         <li>Summarize PDFs upto 5 pages/day.</li>
@@ -15,7 +15,7 @@ const Pricing = () => {
                     </ul>
                     <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2 cursor-pointer'>Login Now</button>
                 </div>
-                <div className='border rounded-2xl py-4 px-12 w-104 hover:shadow-md relative'>
+                <div className='border rounded-2xl py-4 px-12 w-84 lg:w-104 hover:shadow-md relative'>
                     <h4 className='text-lg font-bold text-center mb-4'>Basic <span className='bg-blue-500 text-base font-medium px-2 py-1 rounded-b-md absolute -top-1 right-5 shadow-md shadow-gray-400 text-white '>Most Popular</span></h4>
                     <ul className='list-disc'>
                         <li>Summarize PDFs upto 500 pages/day.</li>
@@ -26,7 +26,7 @@ const Pricing = () => {
                     </ul>
                     <button className='bg-blue-500 text-white font-medium w-1/2 block mx-auto mt-8 mb-4 rounded-full py-2 cursor-pointer'>Login Now</button>
                 </div>
-                <div className='border rounded-2xl py-4 px-12 w-104 hover:shadow-md'>
+                <div className='border rounded-2xl py-4 px-12 w-84 lg:w-104 hover:shadow-md'>
                     <h4 className='text-lg font-bold text-center mb-4'>Professional</h4>
                     <ul className='list-disc'>
                         <li>Summarize PDFs upto 50,000 pages/day.</li>
@@ -39,18 +39,18 @@ const Pricing = () => {
                 </div>
             </div>
             <div>
-                <h1 className='text-xl font-bold mb-8'>Not the plan your looking for?</h1>
-                <div className='w-full border rounded-2xl flex flex-col items-center justify-between py-16 mb-8'>
+                <h1 className='text-xl font-bold mb-8 text-center lg:text-start'>Not the plan your looking for?</h1>
+                <div className='w-84 lg:w-full mx-auto border rounded-2xl flex flex-col items-center justify-between py-16 mb-8'>
                     <h4 className='text-lg font-bold mb-8'>Contact Support Team</h4>
-                    <p className='w-120 mb-2'><span className='text-xl text-red-600'>* </span>Name: </p>
-                    <input type="text" name="name" id="name" className='border border-gray-400 h-12 w-120 rounded-lg px-4 mb-4' required />
-                    <p className='w-120 mb-2'><span className='text-xl text-red-600'>* </span>Email: </p>
-                    <input type="email" name="email" id="email" className='border border-gray-400 h-12 w-120 rounded-lg px-4 mb-4' required />
-                    <p className='w-120 mb-2'><span className='text-xl text-red-600'>* </span>Detailed Query: </p>
-                    <textarea name="query" id="query" rows='8' cols='59' className='border border-gray-400 rounded-lg px-4 py-2 mb-4'></textarea>
-                    <p className='w-118 mt-8'>Or else you can contact us from below email id or mobile number:</p>
-                    <p className='w-118 mt-4'>Email id: <b>contactus@ailegatrix</b></p>
-                    <p className='w-118 mt-4'>Contact number: <b>+91 1234567890</b></p>
+                    <p className='w-80 lg:w-120 mb-2'><span className='text-xl text-red-600'>* </span>Name: </p>
+                    <input type="text" name="name" id="name" className='border border-gray-400 h-12 w-80 lg:w-120 rounded-lg px-4 mb-4' required />
+                    <p className='w-80 lg:w-120 mb-2'><span className='text-xl text-red-600'>* </span>Email: </p>
+                    <input type="email" name="email" id="email" className='border border-gray-400 h-12 w-80 lg:w-120 rounded-lg px-4 mb-4' required />
+                    <p className='w-80 lg:w-120 mb-2'><span className='text-xl text-red-600'>* </span>Detailed Query: </p>
+                    <textarea name="query" id="query" rows='8' className='border border-gray-400 w-80 lg:w-120 rounded-lg px-4 py-2 mb-4'></textarea>
+                    <p className='w-80 lg:w-118 mt-8'>Or else you can contact us from below email id or mobile number:</p>
+                    <p className='w-80 lg:w-118 mt-4'>Email id: <b>contactus@ailegatrix</b></p>
+                    <p className='w-80 lg:w-118 mt-4'>Contact number: <b>+91 1234567890</b></p>
                 </div>
             </div>
         </div>
