0dd1da0b4cf97a3770cd86ed96c1cd534d896650
prajwal <prajwaljcloud@gmail.com>
2026-01-30 22:55:35 +0530

    Created BackgroundDisc and hero section

--- client/src/Pages/Hero.jsx
+++ client/src/Pages/Hero.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Backgroud from './BackgroudDisc'
 
 const Hero = () => {
     return (
-        <div className='container mx-auto bg-amber-200'>Hero</div>
+        <div className='container mx-auto p-20 flex items-center justify-around relative '>
+            <Backgroud height='80px' left='40px' top='10px'/>
+            <div className='bg-amber-200'>
+                <img src="#" alt="#" />
+            </div>
+            <div>
+                <h2 className='text-4xl font-bold mb-8'>Welcome to AI Legatrix!</h2>
+                <p>We would like you to introduce our AI Based Legal documentation assistance.</p>
+                <p>AI Legatrix is a one platform solution for all you legal related query, document summarizer, document generator etc.</p>
+            </div>
+            <Backgroud height='300px' right='200px' bottom='30px' />
+        </div>
     )
 }
 
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import AboutUs from './AboutUs'
 import Pricing from './Pricing'
 import Footer from '../components/Footer'
 import Hero from './Hero'
+import Backgroud from './BackgroudDisc'
 
 const Home = () => {
     return (
